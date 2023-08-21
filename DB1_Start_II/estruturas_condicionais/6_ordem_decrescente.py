c057facbc145f13003c377b5d153e387207c9311
Patricia Rodrigues <patricia.soares.o.rodrigues@gmail.com>
2023-08-21 20:35:23 -0300

    :sparkles: programa que exibe tres numeros digitados em ordem decrescente

---
+++ DB1_Start_II/estruturas_condicionais/6_ordem_decrescente.py
@@ -0,0 +1,16 @@
+a = int(input('Escreva um número: '))
+b = int(input('Escreva um número: '))
+c = int(input('Escreva um número: '))
+
+if a >= b and a >= c and b >= c:
+    print(f'A ordem decrescente é {a} , {b} e {c}')
+elif a >= b and a >= c and c >= b:
+    print(f'A ordem decrescente é {a} , {c} e {b}')
+elif b >= a and b >= c and a >= c:
+    print(f'A ordem decrescente é {b} , {a} e {c}')
+elif b >= a and b >= c and c >= a:
+    print(f'A ordem decrescente é {b} , {c} e {a}')
+elif c >= a and c >= b and a >= b:
+    print(f'A ordem decrescente é {c} , {a} e {b}')
+elif c >= a and c >= b and b >= a:
+    print(f'A ordem decrescente é {c} , {b} e {a}')
