872468909a89d02540be7613fe24b52db1181065
Patricia Rodrigues <patricia.soares.o.rodrigues@gmail.com>
2023-08-21 20:16:11 -0300

    :tada: first commit

---
+++ DB1_Start_I/5_conversor_medidas.py
@@ -0,0 +1,5 @@
+metros = int(input("Metros: "))
+
+cm = (metros*100)
+
+print(f"{metros} metros = {cm} cm")
---
+++ DB1_Start_I/7_calcula_area_quadrado.py
@@ -0,0 +1,7 @@
+lado = float(input("Digite a medida de um dos lados do quadrado: "))
+
+area = (lado * lado)
+dobro = area * 2
+
+print(f"A área do quadrado é: {area}")
+print(f"O dobro da área do quadrado é: {dobro}")
