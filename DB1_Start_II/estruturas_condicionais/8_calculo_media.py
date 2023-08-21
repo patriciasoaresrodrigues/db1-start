c116f27a5fe4964ba7a0f5b0921763282b8da2c5
Patricia Rodrigues <patricia.soares.o.rodrigues@gmail.com>
2023-08-21 20:55:43 -0300

    :sparkles: programa que calcula media e exibe se aprovado

---
+++ DB1_Start_II/estruturas_condicionais/8_calculo_media.py
@@ -0,0 +1,8 @@
+nota1 = int(input("Nota 1: "))
+nota2 = int(input("Nota 2: "))
+media = (nota1+nota2)/2
+
+if media >= 7:
+    print(f'Aprovado com media: {media}')
+else:
+    print(f'Reprovado com media: {media}')
