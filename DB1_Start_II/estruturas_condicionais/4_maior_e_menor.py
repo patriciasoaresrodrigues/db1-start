f35344c0975f1246f7363181dd41e3bbf04b6d54
Patricia Rodrigues <patricia.soares.o.rodrigues@gmail.com>
2023-08-21 20:31:35 -0300

    :sparkles: programa que mostra qual maior e menor numeros digitados

---
+++ DB1_Start_II/estruturas_condicionais/4_maior_e_menor.py
@@ -0,0 +1,10 @@
+n1 = int(input("Informe o primeiro número: "))
+n2 = int(input("Informe o segundo número: "))
+n3 = int(input("Informe o terceiro número: "))
+
+if n1 > n2 and n1 > n3:
+    print(f'O maior número é: {n1}')
+elif n2 > n1 and n2 > n3:
+    print(f'O maior número é: {n2}')
+else:
+    print(f'O maior número é: {n3}')
